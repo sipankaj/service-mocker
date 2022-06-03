768b40abb1ced559c5c12083dda48e36f15664ce
Pankaj Singh <pankajsingh.nitb@gmail.com>
2022-06-04 02:01:18 +0530

    Changes to be committed: 	new file:   artifacts/__files/index.html new file:   artifacts/__files/succes.json 	new file:   artifacts/mappings/mapping.json 	new file:   docker-compose.yaml 	new file:   dockerfile 	new file:   refreshMappings.sh
     Changes not staged for commit:
    	modified:   artifacts/__files/succes.json
    	modified:   artifacts/mappings/mapping.json
    	modified:   docker-compose.yaml
    	modified:   dockerfile
    	modified:   refreshMappings.sh

---
+++ refreshMappings.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Updating Stubs...."
+echo
+type curl >/dev/null 2>&1 || { echo >&2 "Required curl but it's not installed. Aborting."; exit 1; }
+echo
+
+curl -s --request POST http://localhost:7070/__admin/reset
