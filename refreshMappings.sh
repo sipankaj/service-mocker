310466256937594dd2875847195d6ccf6c058a13
sipankaj <pankajsingh.nitb@gmail.com>
2023-08-07 20:53:39 -0400

    Added Docker compose changes

--- refreshMappings.sh
+++ refreshMappings.sh
@@ -5,4 +5,4 @@ echo
 type curl >/dev/null 2>&1 || { echo >&2 "Required curl but it's not installed. Aborting."; exit 1; }
 echo
 
-curl -s --request POST http://localhost:7070/__admin/reset
+curl -s --request POST http://localhost:8080/__admin/reset
